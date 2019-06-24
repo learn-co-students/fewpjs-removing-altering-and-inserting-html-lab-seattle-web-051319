8881247cf9cadb00deb4bdf31dd1feb8f12edb1c
chetchopra <chetanya.chopra94@gmail.com>
2019-06-24 12:11:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("#main").remove()
+
+var newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Blah is the champion"
